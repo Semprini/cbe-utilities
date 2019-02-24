8aa33b86bba8cee87da88284478ae7c4a477c59f
Semprini <atkinpa@gmail.com>
2019-02-24 08:36:01 +0000

    Fix env variables

--- utilities/settings.py
+++ utilities/settings.py
@@ -54,12 +54,11 @@ INSTALLED_APPS = (
     'utilities.usage',
 )
 
-MIDDLEWARE_CLASSES = (
+MIDDLEWARE = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',

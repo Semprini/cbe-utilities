a2381d6427b8f262efdef17b09b5276117d53003
Semprini <atkinpa@gmail.com>
2019-02-28 06:26:17 +0000

    Updated local settings

--- utilities/product/apps.py
+++ utilities/product/apps.py
@@ -6,14 +6,14 @@ class ProductConfig(AppConfig):
 
     def ready(self):
         import drf_nest.signals
-        from utilities.product.models import Product
+        from utilities.product.models import ProductOffering
         from utilities.product.serializers import ProductSerializer
 
         exchange_prefix = settings.MQ_FRAMEWORK['EXCHANGE_PREFIX'] + self.name
         exchange_header_list = ('status',)
         
         post_save.connect(  drf_nest.signals.notify_extra_args(   serializer=ProductSerializer, 
-                                                                exchange_prefix=exchange_prefix + ".Product", 
+                                                                exchange_prefix=exchange_prefix + ".ProductOffering", 
                                                                 exchange_header_list=exchange_header_list)(drf_nest.signals.notify_save_instance), 
-                            sender=Product, weak=False)
+                            sender=ProductOffering, weak=False)
 

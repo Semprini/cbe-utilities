0f853e8e09b5ee6a4905014e203d28ba224ed739
Semprini <atkinpa@gmail.com>
2019-02-28 06:31:23 +0000

    Updated local settings

--- utilities/product/apps.py
+++ utilities/product/apps.py
@@ -1,5 +1,6 @@
 from django.apps import AppConfig
-
+from django.db.models.signals import post_save, post_delete
+from django.conf import settings
 
 class ProductConfig(AppConfig):
     name = 'utilities.product'

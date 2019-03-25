c0385c2572a4059a2882c2c40fe79f6a5f807f6c
Ubuntu <paul@PaulsDev1.lziqttmgjunetbuhmcemywdfta.px.internal.cloudapp.net>
2019-03-25 09:16:47 +0000

    Revert to base server

--- manage_run.sh
+++ manage_run.sh
@@ -6,4 +6,5 @@ bash -c /code/create_local_settings.sh
 sleep 10
 python manage.py migrate
 python manage.py getorcreatesuperuser ${SUNAME} ${SUEMAIL} ${SUPASS}
-uwsgi --ini uwsgi.ini
+#uwsgi --ini uwsgi.ini
+python manage.py runserver 0.0.0.0:8000
